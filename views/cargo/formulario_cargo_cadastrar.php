ea6a7af999e1fa1f4e6dd9018d3e679c179f1ba6
bruno pessoa <brunopessoa.melo@gmail.com>
2020-10-31 20:05:44 -0300

    criando a interface front-end para o view formulario-cargo-cadastrar.php

---
+++ views/cargo/formulario_cargo_cadastrar.php
@@ -0,0 +1,18 @@
+<div class="col-md-offset-4 col-md-4">
+    <form method="post" class="form-group input-group">
+        <fieldset>
+            <legend> Cadastrar Cargo</legend>
+            <label class="control-label"> Nome: </label>
+                <div class="form-group input-group">
+                    <span class="input-group-addon">
+                        <i class="fa fa-briefcase" aria-hidden="true"></i>
+                    </span>
+                    <input type="text" name="cargo" class="form-control" />
+                </div>
+
+                <div class="form-group input-group">
+                    <input type="submit" name="acao" value="Cadastrar Cargo" class="btn btn-sucess">
+                </div>
+        </fieldset>
+    </form>
+</div>
