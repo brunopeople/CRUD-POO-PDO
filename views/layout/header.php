cf3466d2f6017f963cf8866a1b9096b1d078fa87
bruno pessoa <brunopessoa.melo@gmail.com>
2020-11-01 18:08:04 -0300

    criando a interface para a view header.php

---
+++ views/layout/header.php
@@ -0,0 +1,22 @@
+<html>
+    <meta charset="UTF-8">
+    <head>
+        <title> Sistema Gereciamento CRUD</title>
+        <link rel="stylesheet" type="image/png" href="assets/imagens/phpIcon.jpeg"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/application.css" />
+        <!--Bootstrap Core CSS-->
+        <link href="assets/css/template/bootstrap.min.css" rel="stylesheet">
+        <!--MetisMenu CSS-->
+        <link rel="assets/css/template/metisMenu.min.css" rel="stylesheet">
+        <!--bootstrap-social CSS-->
+        <link href="assets/css/template/bootstrap-social.css" rel="stylesheet">
+	  <!-- Custom CSS -->
+	  <link href="assets/css/template/sb-admin-2.css" rel="stylesheet">
+	  <!-- Custom Fonts -->
+	  <link rel="stylesheet" href="assets/css/font/font-awesome/css/font-awesome.min.css">
+
+      <link rel="icon" type="image/png" href="assets/images/phpIcon.jpeg" />
+   <link href="assets/css/application.css" rel="stylesheet">
+    </head>
+  <body>
+</html>
